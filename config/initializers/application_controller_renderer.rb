38c0b9e65403b97409c9dbd4d2f2f92e6f3688a7
kohei-iwase <me.tekelily@gmail.com>
2020-01-30 07:47:40 +0000

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5d30e5b0213ac0ccf4413e1d92697d4910d5ff346939bf140a82534aa5f676984fefa930344495c589ce3ec78d6d01297e14d229af7066aefd422360aefd48ac'
