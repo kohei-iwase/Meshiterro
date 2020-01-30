cde00d275dd6d885a8ad8bc544ed36bb116f7948
kohei-iwase <me.tekelily@gmail.com>
2020-01-30 13:25:43 +0000

    [Finish]5-4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_30_061758) do
+ActiveRecord::Schema.define(version: 2020_01_30_120434) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "coment"
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "comment"
+  end
 
   create_table "post_images", force: :cascade do |t|
     t.text "shop_name"
